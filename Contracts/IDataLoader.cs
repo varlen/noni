817df5704945c7b6b2e5c4a1f11889a47ce1cf93
Varlen Neto <varlenneto@poli.ufrj.br>
2020-08-09 20:21:37 -0300

    Initial commit

---
+++ Contracts/IDataLoader.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace anoni.Contracts {
+
+    public interface IDataLoader
+    {
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using anoni.Contracts;
+
+namespace anoni
+{
+    class Program
+    {
+        IDataLoader dataLoader;
+        IRepresentationExtractor representationExtractor;
+        ITypeMapper typeMapper;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
