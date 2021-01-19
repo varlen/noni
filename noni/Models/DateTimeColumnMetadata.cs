99de318584d3a36a767a70ef9ec89f352e498586
Varlen Neto <varlenneto@poli.ufrj.br>
2021-01-19 17:44:09 -0300

    Remove average and variance from datetime column metadata

--- noni/Models/DateTimeColumnMetadata.cs
+++ noni/Models/DateTimeColumnMetadata.cs
@@ -5,10 +5,8 @@ namespace noni.Models {
     // https://stackoverflow.com/questions/58074304/is-polymorphic-deserialization-possible-in-system-text-json
     public class DateTimeColumnMetadata : ColumnMetadata
     {
-        public DateTime variance{get;set;}
         public DateTime max{get;set;}
         public DateTime min{get;set;}
-        public DateTime average{get;set;}
         public DateTime mode{get; set;}
 
         public DateTimeColumnMetadata(ColumnMetadata defaultMetadata)
