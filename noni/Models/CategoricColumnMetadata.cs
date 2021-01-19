6e44d8a0d1b19bf861c1af282724e93eb16e6442
Varlen Neto <varlenneto@poli.ufrj.br>
2021-01-19 17:43:26 -0300

    Add method to create categories from samples

--- noni/Models/CategoricColumnMetadata.cs
+++ noni/Models/CategoricColumnMetadata.cs
@@ -8,7 +8,35 @@ namespace noni.Models {
     public class CategoricColumnMetadata : ColumnMetadata
     {
         public NamedEntity entityType = NamedEntity.Unknown;
-        public Dictionary<Object, int> categories = new Dictionary<Object, int>();
+        public Dictionary<Object, Int64> categories;
+
+        public CategoricColumnMetadata()
+        {
+            categories = new Dictionary<Object, Int64>();
+        }
+
+        public static Dictionary<Object, Int64> TryCategoriesFromSamples(IEnumerable<string> samples)
+        {
+            var categories = new Dictionary<Object, Int64>();
+            foreach (var item in samples)
+            {
+                if (categories.ContainsKey(item)) 
+                {
+                    categories[item] += 1;
+                }
+                else
+                {
+                    categories.Add(item, 1);
+                }
+            }
+
+            return categories;
+        }
+
+        public CategoricColumnMetadata(Dictionary<Object, Int64> externalCategories)
+        {
+            categories = externalCategories;
+        }
 
         public new decimal distinct{ 
             get {
