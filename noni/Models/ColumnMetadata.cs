a35f85a4fa358d6711f07cd20c68968ce760d0ca
Varlen <varlenneto@poli.ufrj.br>
2021-08-25 22:05:35 -0300

    Add TODO

--- noni/Models/ColumnMetadata.cs
+++ noni/Models/ColumnMetadata.cs
@@ -1,7 +1,11 @@
 using System;
 namespace noni.Models {
 
+    // TODO - Fix metadata serialization
+    // https://stackoverflow.com/questions/58074304/is-polymorphic-deserialization-possible-in-system-text-json    
     // https://stackoverflow.com/questions/58074304/is-polymorphic-deserialization-possible-in-system-text-json
+    // https://stackoverflow.com/questions/29528648/json-net-serialization-of-type-with-polymorphic-child-object
+    // https://github.com/manuc66/JsonSubTypes
     public class ColumnMetadata
     {
         public Int64 distinct{get;set;}
