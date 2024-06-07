07cfd2d9baa9f5fa83c4483cee49365ab1723567
Varlen <varlenneto@poli.ufrj.br>
2024-06-07 18:39:40 -0300

    Update extract script

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,7 +1,7 @@
 export INPUT_DATABASE_URL="postgresql://pguser:password@localhost:5432/sakila"
 export SEMANTIC_MODEL_URL="http://localhost:5000/upload-and-predict"
 export DB_DIALECT="postgres"
-export OUTPUT_FILE="/tmp/output_sakila.json"
+export OUTPUT_FILE="/tmp/output.json"
 
 SCRIPT_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 SCRIPT_PATH="$(cd -- "$SCRIPT_PATH" && pwd)"
